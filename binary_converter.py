97dc53287c9d7ad64c754d280e1f6f292113511e
Lee Spitzley <lspitzley@albany.edu>
2026-02-16 11:10:51 -0500

    blank script

---
+++ binary_converter.py
@@ -0,0 +1,23 @@
+"""
+Code to convert decimal numbers to binary or binary numbers to decimal.
+The numbers should be integers greater than or equal to zero (no decimals). The script 
+should receive the type of conversion and the number to convert 
+as command line arguments.
+"""
+import sys
+
+def decimal_to_binary(n):
+    """Convert a decimal number to binary."""
+    pass
+
+def binary_to_decimal(b):
+    """Convert a binary number to decimal."""
+    pass
+
+def check_arguments():
+    """Check if the command line arguments are valid."""
+    pass
+
+
+if __name__ == "__main__":
+    pass
